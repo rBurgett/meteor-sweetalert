8032042d7f45097d59c5bdf75d6e008ac866811a
Ryan <ryan@burgettweb.net>
2016-01-05 17:30:00 -0500

    Updated git repository

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'smoral:sweetalert',
     summary: "a beautiful replacement for javascript's alert() ",
     version: '1.1.3',
-    git: 'https://github.com/smoral/meteor-sweetalert.git',
+    git: 'https://github.com/kevohagan/meteor-sweetalert.git',
 });
 
 Package.onUse(function(api) {
